b4f33d7a31b1b95623f4b38bd7315c3c6b5f4dc3
Andrey Ferubko <ferubko1999@yandex.ru>
2024-06-11 02:40:20 +0300

    road to mock

--- pkg/worker/worker_test.go
+++ pkg/worker/worker_test.go
@@ -20,43 +20,53 @@ const (
 	source_url = "https://xkcd.com"
 )
 
-func TestWorkerPool(t *testing.T) {
-	words := words.NewWordsStremming()
-
-	cl := xkcd.NewClient(source_url, words)
-
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	data := map[string]xkcd.ComicsInfo{}
-	parallel := 5
-	numIterations := 10
-	filePath := "./../../database.json"
+func readJson(filePath string) (map[string]xkcd.ComicsInfo, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		logrus.Println("Error opening file:", err)
-		return
+		return nil, err
 	}
 	defer file.Close()
 
 	byteValue, err := io.ReadAll(file)
 	if err != nil {
 		logrus.Println("Error reading file:", err)
-		return
+		return nil, err
 	}
 	var expactAll map[string]xkcd.ComicsInfo
 	err = json.Unmarshal(byteValue, &expactAll)
 	if err != nil {
 		logrus.Println("Error unmarshalling JSON:", err)
 	}
-	WorkerPool(cl, numIterations, parallel, &data, ctx, stop)
-	expected := map[string]xkcd.ComicsInfo{}
-	for i := 1; i < numIterations+1; i++ {
-		a := strconv.Itoa(i)
-		expected[a] = expactAll[a]
+	return expactAll, nil
+}
+func TestWorkerPool(t *testing.T) {
+	testData := []struct {
+		parallel      int
+		numIterations int
+	}{
+		{5, 10}, {5, 2}, {100, 4}, {1, 409}, {2, 0}, {2, -1},
 	}
-	if reflect.DeepEqual(data, expected) {
-		t.Log("Test True")
-	} else {
-		t.Log("Test False")
+	filePath := "./../../database.json"
+	expactAll, _ := readJson(filePath)
+	for i := 0; i < len(testData); i++ {
+		words := words.NewWordsStremming()
+		cl := xkcd.NewClient(source_url, words)
+		ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+		data := map[string]xkcd.ComicsInfo{}
+		parallel := testData[i].parallel
+		numIterations := testData[i].numIterations
+		WorkerPool(cl, numIterations, parallel, &data, ctx, stop)
+		expected := map[string]xkcd.ComicsInfo{}
+		for j := 1; j < numIterations+1; j++ {
+			a := strconv.Itoa(j)
+			expected[a] = expactAll[a]
+		}
+		if reflect.DeepEqual(data, expected) {
+			t.Log("Test True")
+		} else {
+			t.Log("Test False")
+		}
 	}
 
 }
