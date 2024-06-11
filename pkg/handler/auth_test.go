8a25105bc21209abb10c4514466e2dcdfeaf0e86
Andrey Ferubko <ferubko1999@yandex.ru>
2024-06-12 01:32:25 +0300

    make all auth tests

--- pkg/handler/auth_test.go
+++ pkg/handler/auth_test.go
@@ -147,7 +147,7 @@ func TestCreateUser(t *testing.T) {
 	testTable := []struct {
 		name               string
 		inputBody          string
-		data               server.UserEntity
+		data               server.User
 		mockBehavior       mockBehavior
 		personalLimit      int
 		personalInterval   time.Duration
@@ -155,5 +155,126 @@ func TestCreateUser(t *testing.T) {
 		rateInterval       time.Duration
 		expectedStatusCode int
 		expectedBody       string
-	}{}
+	}{
+		{
+			name:      "success",
+			inputBody: `{"username":  "andrey",  "password":  "123", "status": "user"}`,
+			data: server.User{
+				Username: "andrey",
+				Password: "123",
+				Status:   "user",
+			},
+			mockBehavior: func(s *mock_service.MockAuth, data server.User) {
+				if err := data.Validate(); err == nil {
+					s.EXPECT().CreateUser(data).Return(nil)
+				}
+			},
+			personalLimit:      5,
+			personalInterval:   time.Second,
+			rateLimit:          500,
+			rateInterval:       time.Second,
+			expectedStatusCode: 200,
+			expectedBody:       "Good",
+		},
+		{
+			name:      "no-status",
+			inputBody: `{"username":  "andrey",  "password":  "123"}`,
+			data: server.User{
+				Username: "andrey",
+				Password: "123",
+			},
+			mockBehavior: func(s *mock_service.MockAuth, data server.User) {
+				if err := data.Validate(); err == nil {
+					s.EXPECT().CreateUser(data).Return(nil)
+				}
+			},
+			personalLimit:      5,
+			personalInterval:   time.Second,
+			rateLimit:          500,
+			rateInterval:       time.Second,
+			expectedStatusCode: 200,
+			expectedBody:       "Good",
+		},
+		{
+			name:      "no-password",
+			inputBody: `{"username":  "andrey",  "password":  ""}`,
+			data: server.User{
+				Username: "andrey",
+				Password: "",
+			},
+			mockBehavior: func(s *mock_service.MockAuth, data server.User) {
+				if err := data.Validate(); err == nil {
+					s.EXPECT().CreateUser(data).Return(nil)
+				}
+			},
+			personalLimit:      5,
+			personalInterval:   time.Second,
+			rateLimit:          500,
+			rateInterval:       time.Second,
+			expectedStatusCode: 400,
+			expectedBody:       errors.New("Unknow status\n").Error(),
+		},
+		{
+			name:      "no-username",
+			inputBody: `{"username":  "",  "password":  "123"}`,
+			data: server.User{
+				Username: "",
+				Password: "123",
+			},
+			mockBehavior: func(s *mock_service.MockAuth, data server.User) {
+				if err := data.Validate(); err == nil {
+					s.EXPECT().CreateUser(data).Return(nil)
+				}
+			},
+			personalLimit:      5,
+			personalInterval:   time.Second,
+			rateLimit:          500,
+			rateInterval:       time.Second,
+			expectedStatusCode: 400,
+			expectedBody:       errors.New("Unknow status\n").Error(),
+		},
+		{
+			name:      "bad-status",
+			inputBody: `{"username":  "andrey",  "password":  "123", "status": "userd"}`,
+			data: server.User{
+				Username: "andrey",
+				Password: "123",
+				Status:   "userd",
+			},
+			mockBehavior: func(s *mock_service.MockAuth, data server.User) {
+				if err := data.Validate(); err == nil {
+					s.EXPECT().CreateUser(data).Return(nil)
+				}
+			},
+			personalLimit:      5,
+			personalInterval:   time.Second,
+			rateLimit:          500,
+			rateInterval:       time.Second,
+			expectedStatusCode: 200,
+			expectedBody:       "Good",
+		},
+	}
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			auth := mock_service.NewMockAuth(c)
+			test.mockBehavior(auth, test.data)
+
+			s := &service.Service{Auth: auth}
+			p := personal_limiter.NewPersonalLimiter(context.Background(), test.personalLimit, test.personalInterval)
+			r := rate_limiter.NewSlidingLogLimiter(test.rateLimit, test.rateInterval)
+			mux := http.NewServeMux()
+			mux.HandleFunc("POST /create", RateCheker(NewHandler(s, r, p).CreateUser, NewHandler(s, r, p), hardSearch, !dominantus))
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/create", bytes.NewBufferString(test.inputBody))
+
+			mux.ServeHTTP(w, req)
+			assert.Equal(t, test.expectedStatusCode, w.Code)
+			assert.Equal(t, test.expectedBody, w.Body.String())
+
+		})
+	}
 }
